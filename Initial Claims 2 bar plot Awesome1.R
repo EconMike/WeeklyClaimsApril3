c14ca5045eae335ed8af356637f94bd820b8bb9a
EconMike <datamike8@gmail.com>
2021-04-15 22:28:07 -0400

    Update Initial Claims 2 bar plot Awesome1.R

--- Initial Claims 2 bar plot Awesome1.R
+++ Initial Claims 2 bar plot Awesome1.R
@@ -11,7 +11,7 @@ library(ggthemes)
 
 ## Two plots
 #set directory
-setwd("     /Weekly Claims")
+setwd("C:/YOUR DIRECTORY/Weekly Claims")
 
 #load data file
 #UI claims seasonal adjusted
@@ -25,7 +25,7 @@ tail(dfo)
 a<-ggplot(dfo, aes(x=DATE, y=Claims)) + 
   geom_bar(stat = "identity",fill = "Black")+
   scale_y_continuous(labels = scales::label_number_si())+
-  scale_x_date(breaks = as.Date(c("2020-03-07", "2021-04-03")),date_breaks = "8 weeks", date_labels = "%b%Y")+
+  scale_x_date(breaks = as.Date(c("2020-03-07", "2021-04-10")),date_breaks = "12 weeks", date_labels = "%b%Y")+
   theme_economist()+
   labs(title = "Initial claims for unemployment insurance", subtitle="Weekly since March 7,2020, seasonally adjusted",
        caption = "Source:Department of Labor")+
@@ -34,8 +34,8 @@ a<-ggplot(dfo, aes(x=DATE, y=Claims)) +
                            plot.subtitle = element_text(size=10,hjust = 0),
                            axis.text.x = element_text(size = 10)
   )+
-  annotate(geom = "text", x = as.Date("2021-02-20") , y = 200000, label = "Week Ending\n April 3: 744K",hjust = "Right", vjust=-2)+
-  annotate("segment", x = as.Date("2021-04-03"), xend = as.Date("2021-04-03"), y = 1800000, yend = 900000)
+  annotate(geom = "text", x = as.Date("2021-03-06") , y = 100, label = "Week Ending\n April 10: 576K",hjust = "Right", vjust=-2)+
+  annotate("segment", x = as.Date("2021-04-10"), xend = as.Date("2021-04-10"), y = 1800000, yend = 900000)
 
 
 #continued claims
@@ -50,18 +50,16 @@ df$DATE<-as.Date(df$DATE)
 b<-ggplot(df, aes(x=DATE, y=CC)) + 
   geom_bar(stat = "identity",fill = "Black")+
   scale_y_continuous(labels = scales::label_number_si())+
-  scale_x_date(breaks = as.Date(c("2020-03-07", "2021-03-27")),date_breaks = "8 weeks", date_labels = "%b%Y")+
+  scale_x_date(breaks = as.Date(c("2020-03-07", "2021-04-03")),date_breaks = "12 weeks", date_labels = "%b%Y")+
   theme_economist()+
   labs(title = "Continuing Claims for Unemployment Insurance",subtitle="Weekly since March 7,2020, seasonally adjusted")+ 
   xlab("") +ylab("")+theme(plot.title=element_text(size = 12), plot.subtitle = element_text(size=10,hjust = 0),
                            axis.text.x = element_text(size = 10))+
-  annotate(geom = "text", x = as.Date("2021-02-06") , y = 900000, label = "Week Ending\n Mar 27: 3.7M",hjust = "Right", vjust=-2)+
-  annotate("segment", x = as.Date("2021-03-27"), xend = as.Date("2021-03-27"), y = 7000000, yend = 5000000)
+  annotate(geom = "text", x = as.Date("2021-03-03") , y = 100, label = "Week Ending\n April 3: 3.7M",hjust = "Right", vjust=-2)+
+  annotate("segment", x = as.Date("2021-04-03"), xend = as.Date("2021-04-03"), y = 7000000, yend = 5000000)
 
 
 
 plot_grid(a, b)
 
-ggsave("T2woClaimsApril8.png")
-
-
+ggsave("T2woClaimsApril15.png")
